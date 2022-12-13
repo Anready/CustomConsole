91a7681eeb658d2c150c24a60e1c8f0a9d8b94f4
Anready <104269567+Anready@users.noreply.github.com>
2022-12-13 16:19:44 +0200

    Add files via upload

---
+++ CustomConsole/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace CustomConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("command.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("command.txt", true))
+                {
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Type 'help' if your don't know command");
+            while(true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("$ - ");
+                Console.ResetColor();
+                String commandIn = Console.ReadLine();
+                if (commandIn == "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Enter command!");
+                    continue;
+                }
+                if (commandIn.Contains("new"))
+                {
+                    commandIn = commandIn.Replace("new ", "");
+                    var arrayOfStrings = commandIn.Split(',');
+                    int i = 0;
+                    using (StreamReader reader = new StreamReader("command.txt"))
+                    {
+                        string text = null;
+                        while ((text = reader.ReadLine()) != null)
+                        {
+                            if (text.Contains(arrayOfStrings[1]) || text.Contains(arrayOfStrings[0]))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("This comand already create");
+                                i = 1;
+                            }
+                        }
+                    }
+                    if(i == 1)
+                    {
+                        continue;
+                    }
+                    writeCommandFile(arrayOfStrings[1], arrayOfStrings[0]);
+                    continue;
+                }
+                if (commandIn.Contains("help"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("This is a custom console, it has only four built-in commands - new, printMyComandList, manyComand and help. \nAll other commands you create yourself through the command in this format: new [name your command], [original command name in console].\nIf your want know your added custom comand, enter printMyComandList.\nIf you want to run all entered commands at the same time write manyComand,\nto get out of it write stop, to run all commands write run.");
+                    continue;
+                }
+                if(commandIn == "printMyComandList")
+                {
+                    using (StreamReader reader = new StreamReader("command.txt"))
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                        continue;
+                    }
+                }
+                if(commandIn == "manyComand")
+                {
+                    Console.WriteLine("Your entered in manyComand mode");
+                    string all = null;
+                    while (true)
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("$ - ");
+                        Console.ResetColor();
+                        commandIn = Console.ReadLine();
+                        if (commandIn == "stop")
+                        {
+                            break;
+                        }
+                        if (commandIn == "run")
+                        {
+                            if(all == null)
+                            {
+                                Console.WriteLine("Don't enter any comand!");
+                                continue;
+                            }
+                            using (StreamWriter writer = new StreamWriter("start.bat", true))
+                            {
+                                writer.WriteLine("@echo off\n" + all + "\ndel %~s0 /q");
+                            }
+                            try
+                            {
+                                Process.Start("start.bat");
+                            }
+                            catch (Exception e)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Command not runing! Error:\n" + e);
+                            }
+                            Console.WriteLine("Your exit from manyComand mode");
+                            break;
+                        }
+                        using (StreamReader reader = new StreamReader("command.txt"))
+                        {
+                            string text = null;
+                            int i = 0;
+                            while ((text = reader.ReadLine()) != null)
+                            {
+                                var arrayOfStrings = text.Split(',');
+                                if (arrayOfStrings[0].Contains(commandIn))
+                                {
+                                    all = all + "\n" + arrayOfStrings[1];
+                                    i = 1;
+                                }
+                            }
+                            if (i == 0)
+                            {
+                                Console.WriteLine("Command not found! Exit from this mode and create this command!");
+                            }
+                        }
+                    }
+                }
+                readCommandFile(commandIn);
+            }
+        }
+        static void writeCommandFile(String Do, String name)
+            {
+            using (StreamWriter writer = new StreamWriter("command.txt", true))
+            {
+                writer.WriteLine(name + " , " + Do);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Create new command successful!");
+            }
+        }
+        static void readCommandFile(String commandIn)
+        {
+            using (StreamReader reader = new StreamReader("command.txt"))
+            {
+                string text = null;
+                int i = 0;
+                while ((text = reader.ReadLine()) != null)
+                {
+                    var arrayOfStrings = text.Split(',');
+                    if (arrayOfStrings[0].Contains(commandIn))
+                    {
+                        Console.WriteLine("");
+                        using (StreamWriter writer = new StreamWriter("start.bat", true))
+                        {
+                            writer.WriteLine("@echo off\n" + arrayOfStrings[1] + "\ndel %~s0 /q");
+                        }
+                        try
+                        {
+                            Process.Start("start.bat");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Command not runing! Error:\n" + e);
+                        }
+                        i = 1;
+                    }
+                }
+                if (i == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Command not found! Create this command!");
+                }
+            }
+        }
+    }
+}
---
+++ CustomConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CustomConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b4674afc-f9f7-494c-bd50-fe276419b4b1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
