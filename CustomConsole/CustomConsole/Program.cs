3f694b00954a8d2969efd607d6d96da92d267e22
Anready <104269567+Anready@users.noreply.github.com>
2022-12-14 12:17:15 +0200

    Add files via upload

--- CustomConsole/CustomConsole/Program.cs
+++ CustomConsole/CustomConsole/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Diagnostics;
 
-
 namespace CustomConsole
 {
     internal class Program
@@ -13,7 +12,6 @@ namespace CustomConsole
             {
                 using (StreamWriter writer = new StreamWriter("command.txt", true))
                 {
-
                 }
             }
             Console.ForegroundColor = ConsoleColor.Green;
@@ -42,7 +40,7 @@ namespace CustomConsole
                         {
                             if (text.Contains(arrayOfStrings[1]) || text.Contains(arrayOfStrings[0]))
                             {
-                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("This comand already create");
                                 i = 1;
                             }
@@ -58,7 +56,7 @@ namespace CustomConsole
                 if (commandIn.Contains("help"))
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("This is a custom console, it has only three built-in commands - new, printMyComandList and help. \nAll other commands you create yourself through the command in this format: new [name your command], [original command name in console].\nIf your want know your added custom comand, enter printMyComandList");
+                    Console.WriteLine("This is a custom console, it has only four built-in commands - new, printMyComandList, manyComand and help. \nAll other commands you create yourself through the command in this format: new [name your command], [original command name in console].\nIf your want know your added custom comand, enter printMyComandList.\nIf you want to run all entered commands at the same time write manyComand,\nto get out of it write stop, to run all commands write run.");
                     continue;
                 }
                 if(commandIn == "printMyComandList")
@@ -69,6 +67,68 @@ namespace CustomConsole
                         continue;
                     }
                 }
+                if(commandIn == "manyComand")
+                {
+                    Console.WriteLine("Your entered in manyComand mode");
+                    string all = null;
+                    while (true)
+                    { 
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("$ - ");
+                        Console.ResetColor();
+                        commandIn = Console.ReadLine();
+                        if (commandIn == "stop")
+                        {
+                            break;
+                        }
+                        if (commandIn == "run")
+                        {
+                            if(all == null)
+                            {
+                                Console.WriteLine("Don't enter any comand!");
+                                continue;
+                            }
+                            using (StreamWriter writer = new StreamWriter("start.bat", true))
+                            {
+                                writer.WriteLine("@echo off\n" + all + "\ndel %~s0 /q");
+                            }
+                            try
+                            {
+                                Process.Start("start.bat");
+                            }
+                            catch (Exception e)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Command not runing! Error:\n" + e);
+                            }
+                            Console.WriteLine("Your exit from manyComand mode");
+                            break;
+                        }
+                        if (commandIn.Contains("cd"))
+                        {
+                            all = all + "\n" + commandIn;
+                            continue;
+                        }
+                        using (StreamReader reader = new StreamReader("command.txt"))
+                        {
+                            string text = null;
+                            int i = 0;
+                            while ((text = reader.ReadLine()) != null)
+                            {
+                                var arrayOfStrings = text.Split(',');
+                                if (arrayOfStrings[0].Contains(commandIn))
+                                {
+                                    all = all + "\n" + arrayOfStrings[1];
+                                    i = 1;
+                                }
+                            }
+                            if (i == 0)
+                            {
+                                Console.WriteLine("Command not found! Exit from this mode and create this command!");
+                            }
+                        }
+                    } continue;
+                }
                 readCommandFile(commandIn);
             }
         }
@@ -76,7 +136,7 @@ namespace CustomConsole
             {
             using (StreamWriter writer = new StreamWriter("command.txt", true))
             {
-                writer.WriteLine(name + " " + Do);
+                writer.WriteLine(name + " , " + Do);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Create new command successful!");
             }
@@ -89,13 +149,13 @@ namespace CustomConsole
                 int i = 0;
                 while ((text = reader.ReadLine()) != null)
                 {
-                    if (text.Contains(commandIn))
+                    var arrayOfStrings = text.Split(',');
+                    if (arrayOfStrings[0].Contains(commandIn))
                     {
-                        text = text.Replace(commandIn, "");
                         Console.WriteLine("");
                         using (StreamWriter writer = new StreamWriter("start.bat", true))
                         {
-                            writer.WriteLine("@echo off\n" + text + "\ndel %~s0 /q");
+                            writer.WriteLine("@echo off\n" + arrayOfStrings[1] + "\ndel %~s0 /q");
                         }
                         try
                         {
